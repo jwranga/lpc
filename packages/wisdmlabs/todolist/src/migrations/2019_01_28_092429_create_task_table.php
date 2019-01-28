6c95aa4c5856f109aab600c6cc2930a2e9bd245f
ranga <ranga@thesanmark.com>
2019-01-28 14:58:25 +0530

    create migration

---
+++ packages/wisdmlabs/todolist/src/migrations/2019_01_28_092429_create_task_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
